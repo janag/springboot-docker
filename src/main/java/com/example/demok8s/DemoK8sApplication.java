c4aa08e030662b081d792f0d271b7cc1a05ca8e8
GJanarthanam@moneygram.com <ajigow@gmail.com>
2018-07-16 21:33:47 -0500

    first commit

---
+++ src/main/java/com/example/demok8s/DemoK8sApplication.java
@@ -0,0 +1,23 @@
+package com.example.demok8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoK8sApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String greet() {
+		return "Hello World";
+	}
+
+
+
+}
